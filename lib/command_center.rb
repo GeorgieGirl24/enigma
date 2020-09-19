855d897c74988ba2a9f8ff3b64b3587cdef3f896
George Soderholm <georgesoderholm@gmail.com>
2020-09-19 13:17:20 -0600

    Add out_put_method

--- lib/command_center.rb
+++ lib/command_center.rb
@@ -1,5 +1,11 @@
 class CommandCenter
-  attr_reader :input_file, :output_file, :key, :date
+  attr_reader :input_file,
+              :output_file,
+              :key,
+              :date,
+              :message,
+              :enigma
+
   def initialize(argv)
     @input_file = argv[0]
     @output_file = argv[1]
@@ -9,5 +15,13 @@ class CommandCenter
     @enigma = Enigma.new
   end
 
-  
+  # def write_to_a_file(output_file, input_message)
+  #   require 'pry';binding.pry
+  #   File.write(output_file, input_message)
+  # end
+
+  def out_put_message
+    "Created '#{@output_file}' with the key #{@key} and date #{@date}"
+  end
+
 end
