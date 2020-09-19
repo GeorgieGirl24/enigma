0722b47d5030ec0c1de4096f871ff56032131c32
George Soderholm <georgesoderholm@gmail.com>
2020-09-19 16:12:23 -0600

    Add squared_number last_four_digits key_values methods

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,2 +1,20 @@
+# this is going to be full of class methods
 class Shift
-end 
+  def self.squared_number(offset)
+    (offset.to_i * offset.to_i).to_s
+  end
+
+  def self.last_four_digits(offset)
+    self.squared_number(offset)[-4..-1]
+  end
+
+  def self.key_values(key)
+    key.chars.each_cons(2).map do |numbers|
+      numbers.join.to_i
+    end
+  end
+
+  def self.find_the_shift
+    
+  end
+end
