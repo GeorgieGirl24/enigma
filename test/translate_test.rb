8e4becec1f4fe5f096a45eb0ee7faa63a4dd62d0
George Soderholm <georgesoderholm@gmail.com>
2020-09-20 09:28:28 -0600

    Add a test to check that a hash is read in from other class

--- test/translate_test.rb
+++ test/translate_test.rb
@@ -16,6 +16,7 @@ class TranslateTest < Minitest::Test
   def test_it_can_set_up_an_alphabet
     expected = ('a'..'z').to_a << ' '
     assert_equal expected, Translate.make_alphabet
+    assert_equal 27, Translate.make_alphabet.count
   end
 
   def test_it_can_find_a_letters_index
@@ -28,7 +29,16 @@ class TranslateTest < Minitest::Test
     assert_equal 'm', Translate.find_indexes_letter(index)
   end
 
-  def test_it_has_a_string_to_read
+  def test_it_can_take_a_return_value_from_shift_class
+    arg_1 = [02, 27, 71, 15]
+    arg_2 = '040895'
+    expected = { A: 3, B: 27, C: 73, D: 20 }
+    Shift.stubs(:find_the_shift).with(arg_1, arg_2).returns(expected)
+
+    assert_equal expected, Translate.get_code_break(arg_1, arg_2)
+  end
+
+  def test_it_has_a_string_to_read_
   end
 
   def test_it_can_take_a_string_and_count
