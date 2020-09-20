40931b65b59d97f0b0fc34e10b2254abaf27694e
George Soderholm <georgesoderholm@gmail.com>
2020-09-19 23:06:46 -0600

    Add four test methods

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -26,13 +26,11 @@ class ShiftTest < Minitest::Test
   end
 
   def test_it_can_split_up_the_key_value
-    # skip
     key = '02715'
     assert_equal [02, 27, 71, 15], Shift.key_values(key)
   end
 
   def test_it_can_add_the_find_the_shift
-    # skip
     number = '040895'
     number_1 = '02715'
     squared_number = Shift.squared_number(number)
@@ -44,5 +42,24 @@ class ShiftTest < Minitest::Test
     key = Shift.key_values(number_1)
     expected = {A: 3, B: 27, C: 73, D: 20}
     assert_equal expected, Shift.find_the_shift(key, offset)
+  end
+
+  def test_it_can_combine_key_and_offset
+    number = '040895'
+    number_1 = '02715'
+    squared_number = Shift.squared_number(number)
+    offset = Shift.last_four_digits(squared_number)
+    key = Shift.key_values(number_1)
+    expected = [[02, 1], [27, 0], [71, 2], [15, 5]]
+    assert_equal expected, Shift.combine_key_and_offset(key, offset)
+  end
+
+  def test_it_can_find_the_sum
+    number = '040895'
+    number_1 = '02715'
+    squared_number = Shift.squared_number(number)
+    offset = Shift.last_four_digits(squared_number)
+    key = Shift.key_values(number_1)
+    assert_equal [3, 27, 73, 20], Shift.find_the_sum(key, offset)
   end
 end
