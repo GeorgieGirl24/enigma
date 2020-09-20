9c5ca3947c777db9fa7f141c9c4e008fec4adf26
George Soderholm <georgesoderholm@gmail.com>
2020-09-20 15:11:45 -0600

    Add a % 27 to deal with the numbers over 26

--- lib/translate.rb
+++ lib/translate.rb
@@ -1,6 +1,7 @@
 require './lib/shift'
 
 class Translate
+  A_SHIFT = (0..1_000_000_000)
   def self.make_alphabet
     ('a'..'z').to_a << ' '
   end
@@ -15,10 +16,21 @@ class Translate
 
   def self.get_code_break(key, shift)
     Shift.find_the_shift(key, shift)
+    # require 'pry';binding.pry
   end
 
-  def self.shift_characters(character, shift, direction)
-    make_alphabet[find_letters_index(character).send(direction, shift)]
+  def self.shift_character(character, shift, direction)
+    make_alphabet[find_letters_index(character).send(direction, shift) % 27]
   end
 
+  def self.translate(text, key, date, direction)
+    seperate_letters = text.each_char.map do |letter|
+      letter
+    end
+    hash = {}
+    seperate_letters.each_with_index do |letter, index|
+      hash[index] = letter
+    end
+    hash
+  end
 end
