e2285573b7c7fc95e454defe525ec6e29005269e
George Soderholm <georgesoderholm@gmail.com>
2020-09-20 12:14:23 -0600

    Add method to shift_characters

--- lib/translate.rb
+++ lib/translate.rb
@@ -13,8 +13,12 @@ class Translate
     make_alphabet[index]
   end
 
-  def self.get_code_break(key, offset)
-    Shift.find_the_shift(key, offset)
-    # require 'pry';binding.pry
+  def self.get_code_break(key, shift)
+    Shift.find_the_shift(key, shift)
   end
+
+  def self.shift_characters(character, shift, direction)
+    make_alphabet[find_letters_index(character).send(direction, shift)]
+  end
+
 end
