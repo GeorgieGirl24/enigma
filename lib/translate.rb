8500d19f6f0ae5e62cf194a21d1af281019e6e57
George Soderholm <georgesoderholm@gmail.com>
2020-09-19 23:48:29 -0600

    Add three methods find to the letters' indexes, find the indexes letters, make an alphabet

--- lib/translate.rb
+++ lib/translate.rb
@@ -1,3 +1,15 @@
 require './lib/shift'
+
 class Translate
+  def self.make_alphabet
+    ('a'..'z').to_a << ' '
+  end
+
+  def self.find_letters_index(letter)
+    make_alphabet.index(letter)
+  end
+
+  def self.find_indexes_letter(index)
+    make_alphabet[index]
+  end
 end
