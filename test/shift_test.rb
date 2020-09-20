84c4947650081b0021a0ac8d9a16924ba9a85bbb
George Soderholm <georgesoderholm@gmail.com>
2020-09-19 22:30:02 -0600

    WIP

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -15,24 +15,33 @@ class ShiftTest < Minitest::Test
   end
 
   def test_it_can_square_a_six_digit_number
-    offset = '040895'
-    assert_equal '1672401025', Shift.squared_number(offset)
+    number = '040895'
+    assert_equal 1672401025, Shift.squared_number(number)
   end
 
   def test_it_can_find_the_last_four_digits
-    offset = '040895'
-    assert_equal 1025, Shift.last_four_digits(offset)
+    number = '040895'
+    squared_number = Shift.squared_number(number)
+    assert_equal 1025, Shift.last_four_digits(squared_number)
   end
 
   def test_it_can_split_up_the_key_value
+    # skip
     key = '02715'
     assert_equal [02, 27, 71, 15], Shift.key_values(key)
   end
 
   def test_it_can_add_the_find_the_shift
-    key = '02715'
-    offset = '1025'
+    # skip
+    number = '040895'
+    number_1 = '02715'
+    squared_number = Shift.squared_number(number)
+    assert_equal 1672401025, Shift.squared_number(number)
 
+    offset = Shift.last_four_digits(squared_number)
+    assert_equal 1025, offset
+
+    key = Shift.key_values(number_1)
     expected = {A: 3, B: 27, C: 73, D: 20}
     assert_equal expected, Shift.find_the_shift(key, offset)
   end
