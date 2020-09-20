025afd2f705e36169eb7ce04a546164421cf8066
George Soderholm <georgesoderholm@gmail.com>
2020-09-19 23:47:06 -0600

    Add test to make alphabet, find the letters index, and find the indexes of a letter

--- test/translate_test.rb
+++ test/translate_test.rb
@@ -5,8 +5,32 @@ require './lib/translate'
 require 'mocha/minitest'
 
 class TranslateTest < Minitest::Test
+  def setup
+    @translate = Translate.new
+  end
+
   def test_it_exists
-    translate = Translate.new
-    assert_instance_of Translate, translate
+    assert_instance_of Translate, @translate
+  end
+
+  def test_it_can_set_up_an_alphabet
+    expected = ('a'..'z').to_a << ' '
+    assert_equal expected, Translate.make_alphabet
+  end
+
+  def test_it_can_find_a_letters_index
+    letter = 'a'
+    assert_equal 0, Translate.find_letters_index(letter)
+  end
+
+  def test_it_can_find_an_indexes_letter
+    index = 12
+    assert_equal 'm', Translate.find_indexes_letter(index)
+  end
+
+  def test_it_has_a_string_to_read
+  end
+
+  def test_it_can_take_a_string_and_count
   end
 end
