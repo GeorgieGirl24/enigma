fd8d51cc863db39f43f1389b2f97dd2a412dd017
George Soderholm <georgesoderholm@gmail.com>
2020-09-21 10:51:34 -0600

    Add a shell of a method, working on specifics

--- lib/command_center.rb
+++ lib/command_center.rb
@@ -11,19 +11,24 @@ class CommandCenter
     @output_file = argv[1]
     @key = argv[2]
     @date = argv[3]
-    @message = File.read(input_file)
+    @message = File.read(@input_file)
     @enigma = Enigma.new
   end
 
+  def method_name
+
+  end
   # def write_to_a_file(output_file, input_message)
   #   require 'pry';binding.pry
   #   File.write(output_file, input_message)
   # end
 
   def encryption_message
-    the_key = @engima.encrypt(@message, @key, @date)[:key]
-    the_date = @enigma.encrypt(@message, @key, @date)[:date]
-    puts "Created '#{@output_file}' with the key #{the_key} and date #{@engima.encrypt[:date]}"
+    # require 'pry';binding.pry
+    p ARGV[0]
+    @message
+    # @engima.encrypt(@message, @key, @date)
+    puts "Created '#{@output_file}' with the key #{@the_key} and date #{@the_date}"
   end
 
   def decryption_message
