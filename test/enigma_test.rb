125f8816c4a364a743f5d0ad33757c4eddd363f3
George Soderholm <georgesoderholm@gmail.com>
2020-09-20 19:40:09 -0600

    Remove extra lines in the setup

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -21,7 +21,7 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_it_can_generate_a_random_five_number_code
-    assert_includes(1..99999, 5) #@enigma.find_random_number
+    assert_includes(1..99999, 5)
   end
 
   def test_it_can_make_an_encryption_with_three_arguments
@@ -74,13 +74,21 @@ class EnigmaTest < Minitest::Test
       key: '02715',
       date: '040895'
     }
+    expected_1 = {
+      decryption: 'hello world!',
+      key: '34783',
+      date: '040895'
+    }
     assert_equal expected, @enigma.decrypt('keder ohulw!', '02715', '040895')
+    refute_equal expected_1, @enigma.decrypt('hello world!', '02715', '040895')
   end
 
   def test_it_can_decrypt_with_a_no_date
-    @enigma.stubs(:find_todays_date).returns('040895')
-    arguments = ['keder ohulw!', '02715']
-    Translate.stubs(:return_message).with(*arguments).returns('hello world!')
+    # @enigma.stubs(:find_todays_date).returns('040895')
+    date = @enigma.find_todays_date
+    string = 'keder ohulw!'
+    key = '02715'
+    Translate.stubs(:return_message).with(string, key, date).returns('hello world!')
     expected = {
       decryption: 'hello world!',
       key: '02715',
