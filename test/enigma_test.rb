cca8d438f35650259be76ed972c6dcbde48e2106
George Soderholm <georgesoderholm@gmail.com>
2020-09-20 19:15:15 -0600

    Add refute equal to test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -25,39 +25,45 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_it_can_make_an_encryption_with_three_arguments
-    arguments = ['hello world', '02715', '040895']
+    arguments = ['hello world!', '02715', '040895']
     Translate.stubs(:return_message).with(arguments).returns('keder ohulw!')
     expected = {
       encryption: 'keder ohulw!',
       key: '02715',
       date: '040895'
     }
-    assert_equal expected, @enigma.encrypt('keder ohulw!', '02715', '040895')
+    assert_equal expected, @enigma.encrypt('hello world!', '02715', '040895')
   end
 
   def test_it_can_make_an_encryption_with_two_arguments
     @enigma.stubs(:find_todays_date).returns('040895')
-    arguments = ['hello world', '02715']
+    arguments = ['hello world!', '02715']
     Translate.stubs(:return_message).with(*arguments).returns('keder ohulw!')
     expected = {
       encryption: 'keder ohulw!',
       key: '02715',
       date: '040895'
     }
-    assert_equal expected, @enigma.encrypt('keder ohulw!', '02715', '040895')
+    assert_equal expected, @enigma.encrypt('hello world!', '02715', '040895')
   end
 
   def test_it_can_make_an_encryption_with_one_arguments
     @enigma.stubs(:find_random_number).returns('02715')
-    @enigma.stubs(:find_todays_date).returns('040895')
-    arguments = ['hello world']
+    @enigma.stubs(:find_todays_date).returns('190920')
+    arguments = ['hello world!']
     Translate.stubs(:return_message).with(*arguments).returns('keder ohulw!')
     expected = {
-      encryption: 'keder ohulw!',
+      encryption: 'pib wdmczpu!',
+      key: '02715',
+      date: '190920'
+    }
+    expected_1 = {
+      encryption: 'newyu gaxlo!',
       key: '02715',
       date: '040895'
     }
-    assert_equal expected, @enigma.encrypt('keder ohulw!', '02715', '040895')
+    assert_equal expected, @enigma.encrypt('hello world!', '02715', '190920')
+    refute_equal expected_1, @enigma.encrypt('hello world!', '02715', '040895')
   end
 
   def test_it_can_decrypt_with_a_key_and_date
@@ -68,7 +74,7 @@ class EnigmaTest < Minitest::Test
       key: '02715',
       date: '040895'
     }
-    assert_equal expected, @enigma.decrypt('hello world!', '02715', '040895')
+    assert_equal expected, @enigma.decrypt('keder ohulw!', '02715', '040895')
   end
 
   def test_it_can_decrypt_with_a_no_date
@@ -80,8 +86,12 @@ class EnigmaTest < Minitest::Test
       key: '02715',
       date: '040895'
     }
-    assert_equal expected, @enigma.decrypt('hello world!', '02715', '040895')
+    expected_1 = {
+      decryption: 'hello world!',
+      key: '02715',
+      date: '190920'
+    }
+    assert_equal expected, @enigma.decrypt('keder ohulw!', '02715', '040895')
+    refute_equal expected_1, @enigma.decrypt('keder ohulw!', '02715', '040895')
   end
-
-
 end
