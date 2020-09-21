2a9646ddcc66f4d3b9e02e927cac04fbbffd087a
George Soderholm <georgesoderholm@gmail.com>
2020-09-21 12:29:15 -0600

    Edit method to get needed information

--- lib/command_center.rb
+++ lib/command_center.rb
@@ -11,27 +11,24 @@ class CommandCenter
     @output_file = argv[1]
     @key = argv[2]
     @date = argv[3]
-    @message = File.read(@input_file)
+    @message = File.read(input_file)
     @enigma = Enigma.new
   end
 
-  def method_name
-
-  end
   # def write_to_a_file(output_file, input_message)
-  #   require 'pry';binding.pry
   #   File.write(output_file, input_message)
   # end
-
-  def encryption_message
-    # require 'pry';binding.pry
-    p ARGV[0]
-    @message
-    # @engima.encrypt(@message, @key, @date)
-    puts "Created '#{@output_file}' with the key #{@the_key} and date #{@the_date}"
+  def encrypt_pattern
+      # require 'pry';binding.pry
+    @engima.encrypt(@message)
+    # encryption_message
   end
 
-  def decryption_message
-    puts "Created '#{@output_file}' with the key #{@key} and date #{@date}"
-  end
+  # def encryption_message
+  #   puts "Created '#{@output_file}' with the key #{encrypt_pattern[:key]} and date #{encrypt_pattern[:date]}"
+  # end
+  #
+  # def decryption_message
+  #   puts "Created '#{@output_file}' with the key #{@key} and date #{@date}"
+  # end
 end
